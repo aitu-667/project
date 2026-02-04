64caa0fee2daa41ac6f73486c97531484e205afa
aitu-667 <nurkenmamaj@gmail.com>
2026-02-04 23:31:58 +0500

    Delete Borrowing.java

--- Borrowing.java
+++
@@ -1,9 +0,0 @@
-package model;
-import java.time.LocalDate;
-public class Borrowing {
-    public int id;
-    public int userId;
-    public int bookId;
-    public LocalDate borrowDate;
-    public LocalDate returnDate;
-}
