e725706961d7dcfb9fb38a576c785043da969eff
aitu-667 <nurkenmamaj@gmail.com>
2026-02-04 23:34:44 +0500

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-import controller.LibraryController;
-public class Main {
-    public static void main(String[] args) throws Exception {
-        LibraryController controller=new LibraryController();
-        controller.start();
-    }
-}
