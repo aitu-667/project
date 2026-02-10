632aafaecc357dd794b26eb43d4a573aaa8af21a
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:53:49 +0500

    Update FullBookDTO.java

--- FullBookDTO.java
+++ FullBookDTO.java
@@ -3,13 +3,13 @@ package dto;
 public class FullBookDTO {
     public int id;
     public String title;
+    public String author;
     public String category;
 
-    @Override
-    public String toString() {
-        return "ID: " + id +
-                "\nTitle: " + title +
-                "\nCategory: " + category +
-                "\n---------------------";
+    public FullBookDTO(int id, String title, String author, String category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
     }
 }
