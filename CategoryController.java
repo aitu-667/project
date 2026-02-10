fd5e171dda94403ab9ba078fea817e040ab1945c
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:51:46 +0500

    Update CategoryController.java

--- CategoryController.java
+++ CategoryController.java
@@ -1,14 +1,17 @@
-package controller;
+package Controller;
 
 import service.CategoryService;
+
 import java.util.Scanner;
 
 public class CategoryController {
+
     private final CategoryService service = new CategoryService();
 
-    public void addCategory() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter category name: ");
-        service.addCategory(sc.nextLine());
+    public void addCategory(Scanner sc) {
+        System.out.print("Category name: ");
+        String name = sc.nextLine();
+        service.addCategory(name);
+        System.out.println("Category added.");
     }
 }
