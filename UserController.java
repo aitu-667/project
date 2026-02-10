bfd438335f6702aa77a04bf87f47b9b80334557a
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:55:39 +0500

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,4 +1,4 @@
-package controller;
+package Controller;
 
 import entity.User;
 import service.UserService;
@@ -7,42 +7,54 @@ import java.util.Scanner;
 
 public class UserController {
 
-    private final UserService service = new UserService();
-    private final Scanner sc = new Scanner(System.in);
+    private final UserService userService = new UserService();
+    private final BookController bookController;
+    private final CategoryController categoryController;
 
-    public void login() {
+    public UserController(BookController b, CategoryController c) {
+        this.bookController = b;
+        this.categoryController = c;
+    }
+
+    public void login(Scanner sc) {
         System.out.print("Username: ");
-        User user = service.login(sc.nextLine());
+        String username = sc.nextLine();
+
+        User user = userService.login(username);
+        if (user == null) {
+            System.out.println("Invalid user");
+            return;
+        }
 
         System.out.println("Login successful!");
-        System.out.println("Role: " + user.role);
-
-        if (user.role.equals("ADMIN"))
-            adminMenu();
-        else
-            userMenu();
+        System.out.println("Role: " + user.getRole());
+        adminMenu(sc);
     }
 
-    private void adminMenu() {
-        System.out.println("""
-            --- ADMIN MENU ---
-            1. Add book
-            2. Add category
-            3. Remove book
-            4. View all users
-            0. Logout
-            Choose option:
-            """);
-    }
+    private void adminMenu(Scanner sc) {
+        while (true) {
+            System.out.println("--- ADMIN MENU ---");
+            System.out.println("1. Add book");
+            System.out.println("2. Add category");
+            System.out.println("3. Remove book");
+            System.out.println("4. View all users");
+            System.out.println("0. Logout");
+            System.out.print("Choose option: ");
 
-    private void userMenu() {
-        System.out.println("""
-            --- USER MENU ---
-            1. Borrow book
-            2. Return book
-            3. View my borrowed books
-            0. Logout
-            Choose option:
-            """);
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1 -> bookController.addBook(sc);
+                case 2 -> categoryController.addCategory(sc);
+                case 3 -> bookController.removeBook(sc);
+                case 4 -> userService.getAllUsers()
+                        .forEach(u -> System.out.println(u.getUsername()));
+                case 0 -> {
+                    System.out.println("Logged out.");
+                    return;
+                }
+            }
+        }
     }
 }
