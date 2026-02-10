d9a2628d9a6560e714dcb00830809dae431f3d09
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:48:58 +0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,8 +1,20 @@
 package entity;
 
 public class Book {
-    public int id;
-    public String title;
-    public int categoryId;
-    public boolean borrowed;
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+
+    public Book(int id, String title, String author, String category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getCategory() { return category; }
 }
