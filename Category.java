5a1ddeb61a838f5db466434a41fd0ebe59cde27d
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:51:16 +0500

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,6 +1,14 @@
 package entity;
 
 public class Category {
-    public int id;
-    public String name;
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
 }
