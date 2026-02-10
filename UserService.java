351d426d840720bec2ac7abba64f2205364c9971
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:56:32 +0500

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -3,8 +3,15 @@ package service;
 import entity.User;
 import repository.RepositoryFactory;
 
+import java.util.List;
+
 public class UserService {
+
     public User login(String username) {
-        return RepositoryFactory.userRepo().login(username);
+        return RepositoryFactory.getUserRepository().findByUsername(username);
+    }
+
+    public List<User> getAllUsers() {
+        return RepositoryFactory.getUserRepository().findAll();
     }
 }
