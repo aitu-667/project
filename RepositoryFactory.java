989a9ab31fa9f29d9e528db65ddc2f08b1e060c2
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:54:45 +0500

    Update RepositoryFactory.java

--- RepositoryFactory.java
+++ RepositoryFactory.java
@@ -1,13 +1,26 @@
 package repository;
 
 public class RepositoryFactory {
-    public static BookRepository bookRepo() {
-        return new BookRepository();
+
+    private static BookRepository bookRepository;
+    private static CategoryRepository categoryRepository;
+    private static UserRepository userRepository;
+
+    public static BookRepository getBookRepository() {
+        if (bookRepository == null)
+            bookRepository = new BookRepository();
+        return bookRepository;
     }
-    public static CategoryRepository categoryRepo() {
-        return new CategoryRepository();
+
+    public static CategoryRepository getCategoryRepository() {
+        if (categoryRepository == null)
+            categoryRepository = new CategoryRepository();
+        return categoryRepository;
     }
-    public static UserRepository userRepo() {
-        return new UserRepository();
+
+    public static UserRepository getUserRepository() {
+        if (userRepository == null)
+            userRepository = new UserRepository();
+        return userRepository;
     }
 }
