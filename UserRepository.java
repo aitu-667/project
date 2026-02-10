885f9f2b18569ec0ebb3531c7984877c8836e6a6
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:56:08 +0500

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -2,12 +2,27 @@ package repository;
 
 import entity.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
 
-    public User login(String username) {
-        User u = new User();
-        u.username = username;
-        u.role = username.equals("admin") ? "ADMIN" : "USER";
-        return u;
+    private static final List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "admin", "ADMIN"));
+    }
+
+    public User findByUsername(String username) {
+        for (User u : users) {
+            if (u.getUsername().equalsIgnoreCase(username)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public List<User> findAll() {
+        return users;
     }
 }
