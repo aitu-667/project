8b89783f6005b2c2e0a715fc6b439b0d6c239d86
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:53:23 +0500

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,30 +5,22 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static DBConnection instance;
-    private Connection connection;
+    private static Connection connection;
 
-    private static final String URL =
-            "jdbc:postgresql://localhost:5432/ass5";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "nurken08"; // pgAdmin пароль
+    private DBConnection() {}
 
-    private DBConnection() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(
+                        "jdbc:postgresql://localhost:5432/oop+ass5",
+                        "postgres",
+                        "nurken08"
+                );
+            } catch (Exception e) {
+                System.out.println("DB connection failed");
+            }
         }
-    }
-
-    public static DBConnection getInstance() {
-        if (instance == null)
-            instance = new DBConnection();
-        return instance;
-    }
-
-    public Connection getConnection() {
         return connection;
     }
 }
