397df307fde76ebfbec7f74f9bc6077f3ca52b9f
aitu-667 <nurkenmamaj@gmail.com>
2026-02-10 09:50:43 +0500

    Update BookService.java

--- BookService.java
+++ BookService.java
@@ -1,12 +1,32 @@
+
 package service;
 
+import dto.FullBookDTO;
+import entity.Book;
 import repository.RepositoryFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookService {
 
-    public void showBooks() {
-        RepositoryFactory.bookRepo()
-                .findAll()
-                .forEach(System.out::println); // lambda
+    public List<Book> getAllBooks() {
+        return RepositoryFactory.getBookRepository().findAll();
+    }
+
+    public void addBook(String title, String author, String category) {
+        int id = getAllBooks().size() + 1;
+        RepositoryFactory.getBookRepository()
+                .save(new Book(id, title, author, category));
+    }
+
+    public boolean removeBook(int id) {
+        return RepositoryFactory.getBookRepository().deleteById(id);
+    }
+
+    public FullBookDTO getBookDetails(int id) {
+        Book b = RepositoryFactory.getBookRepository().findById(id);
+        if (b == null) return null;
+        return new FullBookDTO(b.getId(), b.getTitle(), b.getAuthor(), b.getCategory());
     }
 }
